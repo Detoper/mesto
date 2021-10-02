74b05be1a3ebad7e0ad722a6f0bdfaa6f456711e
VladON <vladislavcheg1996@yandex.ru>
2021-10-02 07:48:36 +0300

    добавлен скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,87 @@
-let profile = document.querySelector('.profile');
-let editButton = profile.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__close-button');
-let popupName = popup.querySelector('.popup__input_type_name');
-let popupDescription = popup.querySelector('.popup__input_type_description');
-let profileTitle = profile.querySelector('.profile__title');
-let profileSubtitle = profile.querySelector('.profile__subtitle');
+//массив начальных данных
+const initialCards = [{
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+//кнопки из профиля, данные профиля
+const profile = document.querySelector('.profile');
+const editButton = profile.querySelector('.profile__edit-button');
+const addButton = profile.querySelector('.profile__add-button');
+const profileTitle = profile.querySelector('.profile__title');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
+//попап профиля
+const profilePopup = document.querySelector('.popup_type_profile');
+const popupName = profilePopup.querySelector('.popup__input_type_name');
+const popupDescription = profilePopup.querySelector('.popup__input_type_description');
+const profCloseButton = profilePopup.querySelector('.popup__close-button');
+//попап добавления картинки
+const imgPopup = document.querySelector('.popup_type_image');
+const popupPlaceName = imgPopup.querySelector('.popup__input_type_place-name');
+const popupLink = imgPopup.querySelector('.popup__input_type_link');
+const imgCloseButton = imgPopup.querySelector('.popup__close-button');
+//попап увеличения картинки
+const largeImgPopup = document.querySelector('.popup_type_large-image');
+const largeImg = largeImgPopup.querySelector('.popup__large-img');
+const largeImgTitle = largeImgPopup.querySelector('.popup__img-title');
+const largeImgCloseButton = largeImgPopup.querySelector('.popup__close-button');
+//грид-галерея
+const gridGallery = document.querySelector('.grid-gallery');
+const cardTemplate = gridGallery.querySelector('#card').content;
+//функция добавления картинки (используется в следующей за ней функции)
+function addElement(Element) {
+    gridGallery.prepend(Element);
+}
+const createElement = (name, link) => {
+        const cardElement = cardTemplate.querySelector('.grid-gallery__card').cloneNode(true);
+        const cardImg = cardElement.querySelector('.grid-gallery__card-img');
+        cardImg.src = link;
+        cardImg.alt = name;
+        cardElement.querySelector('.grid-gallery__card-title').textContent = name;
+        //слушатель лайка
+        cardElement.querySelector('.grid-gallery__card-like').addEventListener('click', function(evt) {
+            evt.target.classList.toggle('grid-gallery__card-like_active');
+        });
+        //слушатель корзины
+        cardElement.querySelector('.grid-gallery__card-trash').addEventListener('click', function(evt) {
+            evt.target.closest('.grid-gallery__card').remove();
+        });
+        //слушатель на открытие картинки
+        cardElement.querySelector('.grid-gallery__card-img').addEventListener('click', function(evt) {
+            const text = evt.target.closest('.grid-gallery__card').querySelector('.grid-gallery__card-title').textContent;
+            largeImg.src = evt.target.src;
+            largeImg.alt = text;
+            largeImgTitle.textContent = text;
+            largeImgPopup.classList.add('popup_opened');
+        })
 
-function openPopup() {
+        addElement(cardElement);
+    }
+    //действия с попапом профиля
+function openProfPopup() {
     popupName.value = profileTitle.textContent.trim();
     popupDescription.value = profileSubtitle.textContent.trim();
-    popup.classList.add('popup_opened');
-}
-
-function closePopup() {
-    popup.classList.remove('popup_opened');
+    profilePopup.classList.add('popup_opened');
 }
 
 function savePopup(evt) {
@@ -23,7 +90,34 @@ function savePopup(evt) {
     profileSubtitle.textContent = popupDescription.value;
     closePopup();
 }
+//действия с попапом картинки
+function openImgPopup() {
+    imgPopup.classList.add('popup_opened');
+}
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-popup.addEventListener('submit', savePopup);
+function addPicture(evt) {
+    evt.preventDefault();
+    createElement(popupPlaceName.value, popupLink.value);
+    closePopup();
+    popupPlaceName.value = '';
+    popupLink.value = '';
+}
+//общий клоуз
+function closePopup() {
+    profilePopup.classList.remove('popup_opened');
+    imgPopup.classList.remove('popup_opened');
+    largeImgPopup.classList.remove('popup_opened');
+}
+//слушатели
+editButton.addEventListener('click', openProfPopup);
+addButton.addEventListener('click', openImgPopup);
+profCloseButton.addEventListener('click', closePopup);
+imgCloseButton.addEventListener('click', closePopup);
+largeImgCloseButton.addEventListener('click', closePopup);
+profilePopup.addEventListener('submit', savePopup);
+imgPopup.addEventListener('submit', addPicture);
+//цикл добавления при загрузке страницы
+
+for (let i = 0; i < initialCards.length; ++i) {
+    createElement(initialCards[i].name, initialCards[i].link);
+}
